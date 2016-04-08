cf2dd54242fe3771de885944620ec709b3a7d3e1
Grzegorz Budzyn <budzyn.grzegorz.it@gmail.com>
2016-04-08 20:29:08 +0200

    tinyMce improvment

---
+++ DomOpieki/public/js/addTinyMce.js
@@ -0,0 +1,11 @@
+/**
+ * Created by GrzegorzOLD on 4/8/2016.
+ */
+
+    tinymce.init({
+        selector: '[name="description"]',
+        plugins: [             'autoresize','advlist autolink lists link image charmap print preview hr anchor pagebreak',             'searchreplace wordcount visualblocks visualchars code fullscreen',             'insertdatetime media nonbreaking save table contextmenu directionality',             'emoticons template paste textcolor colorpicker textpattern imagetools'         ],
+        width: '100%',
+        height: 400,
+        autoresize_min_height: 400,
+    });

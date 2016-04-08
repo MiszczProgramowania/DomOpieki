6515418559cf607322ca0fcacf4f7432e6b39dea
Grzegorz Budzyn <budzyn.grzegorz.it@gmail.com>
2016-04-08 21:31:30 +0200

    extended tinyMce

--- DomOpieki/public/js/addTinyMce.js
+++ DomOpieki/public/js/addTinyMce.js
@@ -4,9 +4,12 @@
 
     tinymce.init({
         selector: '[name="description"]',
-        plugins: [             'autoresize','advlist autolink lists link image charmap print preview hr anchor pagebreak',             'searchreplace wordcount visualblocks visualchars code fullscreen',             'insertdatetime media nonbreaking save table contextmenu directionality',             'emoticons template paste textcolor colorpicker textpattern imagetools'         ],
+        plugins: [             'autosave autoresize','advlist autolink lists link image charmap print preview hr anchor pagebreak',             'searchreplace wordcount visualblocks visualchars code fullscreen',             'insertdatetime media nonbreaking save table contextmenu directionality',             'emoticons template paste textcolor colorpicker textpattern imagetools'         ],
         width: '100%',
         height: 400,
         autoresize_min_height: 400,
-        language : 'pl'
+        language : 'pl',
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify ',
+        toolbar2: 'print preview media | forecolor backcolor emoticons | bullist numlist outdent indent | link image',
+        image_advtab: true
     });

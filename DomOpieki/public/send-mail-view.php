0ac4f893aeedcc061dd0638c763370545ba81313
Grzegorz Budzyn <budzyn.grzegorz.it@gmail.com>
2016-05-28 22:34:06 +0200

    contact form

---
+++ DomOpieki/public/send-mail-view.php
@@ -0,0 +1,20 @@
+
+
+<form class="brazowy" name="contactform" method="post" action="send_form_email.php">
+    <div class="form-group">
+        <input id="exampleInputname" class="form-control" placeholder="Imie" type="text" name="first_name">
+    </div>
+    <div class="form-group">
+        <input id="exampleInputname" class="form-control" placeholder="Nazwisko" type="text" name="last_name">
+    </div>
+    <div class="form-group">
+        <input type="text" name="email" id="exampleInputEmail1" class="form-control" placeholder="Adres e-mail" />
+    </div>
+    <div class="form-group">
+        <input type="text" name="phone" id="exampleInputEmail1" class="form-control" placeholder="Telefon" />
+    </div>
+    <div class="form-group">
+        <textarea name="comments" id="exampleInputtemat1" class="form-control" rows="6" placeholder="Tresc wiadomosci">&nbsp;</textarea>
+    </div>
+    <input class="btn btn-default" type="submit" value="Submit">
+</form>
